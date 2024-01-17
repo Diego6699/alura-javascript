0fc99d0407cc3481fc78b8ab876e6341b6991f7b
Diego Leonardo <diegoleonardo9966@gmail.com>
2024-01-17 11:20:38 -0300

    tratamento com if

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ alert('Bem vindo o jogo do número secreto!');
 let numeroSecreto = 29;
 
 let chute = prompt('Escolha um número entre 1 e 30');
+
+if (numeroSecreto == chute) {
+    console.log('Isso ai! descobriu o número secreto ');
+}
